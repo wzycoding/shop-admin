db068ea36af600d0f0549f531953aa8ca3b6c78e
wzy <842089160@qq.com>
2020-03-11 23:00:26 +0800

    初始化权限数据

---
+++ src/main/resources/db/migration/V3__add_sys_acl_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `sys_acl` VALUES (1, '添加用户', '/user/add', 1, '2020-02-24 18:36:34', '2020-02-24 18:36:38', 0);
+INSERT INTO `sys_acl` VALUES (2, '删除用户', '/user/delete', 3, '2020-02-24 18:37:29', '2020-02-24 18:37:33', 0);
+INSERT INTO `sys_acl` VALUES (3, '查询用户', '/user/find', 2, '2020-02-24 18:37:56', '2020-02-24 18:37:59', 0);
